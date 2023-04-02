3ed8a3f59985bc54ac6cdc92c65057a5db98e143
DabeenKim <81741760+dakong8@users.noreply.github.com>
2023-04-02 21:03:32 +0900

    test: 배포 4차 테스트

--- config/index.js
+++ config/index.js
@@ -52,6 +52,8 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "",
+
     /**
      * Source Maps
      */
@@ -72,5 +74,7 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-  }
+  },
+
+  publicPath: process.env.NODE_ENV === "production" ? "./" : ""
 }
