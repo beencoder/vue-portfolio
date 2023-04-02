fe544ad30cbc19ed20b2098efd76d8a7b18153e5
DabeenKim <81741760+dakong8@users.noreply.github.com>
2023-04-02 21:33:29 +0900

    test: 배포 6차 테스트

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "./" : "",
+    publicPath: process.env.NODE_ENV === "production" ? "vue-project/" : "",
     chainWebpack: config => {
         config.module.rules.delete("eslint");
     }
